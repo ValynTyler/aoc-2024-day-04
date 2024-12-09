cd813662784aa34fc287140e88be1a2156cdf9ac
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 18:46:32 +0200

    update the way directions are stored

--- src/bin/part1/xmas_direction.rs
+++ src/bin/part1/xmas_direction.rs
@@ -10,55 +10,89 @@ pub struct XmasDirections {
 }
 
 impl XmasDirections {
+    fn add(&mut self, dir: XmasDirection) {
+        match dir {
+            XmasDirection::North => self.north = true,
+            XmasDirection::South => self.south = true,
+            XmasDirection::East => self.east = true,
+            XmasDirection::West => self.west = true,
+            XmasDirection::NorthEast => self.north_east = true,
+            XmasDirection::NorthWest => self.north_west = true,
+            XmasDirection::SouthEast => self.south_east = true,
+            XmasDirection::SouthWest => self.south_west = true,
+        }
+    }
+
+    fn directions(&self) -> Vec<XmasDirection> {
+        let mut v = vec![];
+
+        if self.north == true { v.push(XmasDirection::North) }
+        if self.south == true { v.push(XmasDirection::South) }
+        if self.east == true { v.push(XmasDirection::East) }
+        if self.west == true { v.push(XmasDirection::West) }
+        if self.north_east == true { v.push(XmasDirection::NorthEast) }
+        if self.north_west == true { v.push(XmasDirection::NorthWest) }
+        if self.south_east == true { v.push(XmasDirection::SouthEast) }
+        if self.south_west == true { v.push(XmasDirection::SouthWest) }
+
+        v
+    }
+}
+
+pub enum XmasDirection {
+    North,
+    South,
+    East,
+    West,
+    NorthEast,
+    NorthWest,
+    SouthEast,
+    SouthWest,
+}
+
+impl XmasDirection {
     pub fn deltas(&self) -> Vec<(isize, isize)> {
-        // XmasDirections::North => vec![
-        //     ( 0,  0),
-        //     ( 0,  1),
-        //     ( 0,  2),
-        //     ( 0,  3),
-        // ],
-        // XmasDirections::North_West => vec![
-        //     (-0,  0),
-        //     (-1,  1),
-        //     (-2,  2),
-        //     (-3,  3),
-        // ],
-        // XmasDirections::West => vec![
-        //     (-0,  0),
-        //     (-1,  0),
-        //     (-2,  0),
-        //     (-3,  0),
-        // ],
-        // XmasDirections::South_West => vec![
-        //     (-0, -0),
-        //     (-1, -1),
-        //     (-2, -2),
-        //     (-3, -3),
-        // ],
-        // XmasDirections::South => vec![
-        //     ( 0, -0),
-        //     ( 0, -1),
-        //     ( 0, -2),
-        //     ( 0, -3),
-        // ],
-        // XmasDirections::South_East => vec![
-        //     ( 0,  0),
-        //     (-1,  1),
-        //     (-2,  2),
-        //     (-3,  3),
-        // ],
-        // XmasDirections::East => vec![
-        //     ( 0,  0),
-        //     ( 0,  1),
-        //     ( 0,  2),
-        //     ( 0,  3),
-        // ],
-        // XmasDirections::North_East => vec![
-        //     ( 0,  0),
-        //     ( 1,  1),
-        //     ( 2,  2),
-        //     ( 3,  3),
-        // ],
-        todo!()
+        match self {
+            XmasDirection::North => vec![
+                ( 0,  1),
+                ( 0,  2),
+                ( 0,  3),
+            ],
+            XmasDirection::NorthWest => vec![
+                (-1,  1),
+                (-2,  2),
+                (-3,  3),
+            ],
+            XmasDirection::West => vec![
+                (-1,  0),
+                (-2,  0),
+                (-3,  0),
+            ],
+            XmasDirection::SouthWest => vec![
+                (-1, -1),
+                (-2, -2),
+                (-3, -3),
+            ],
+            XmasDirection::South => vec![
+                ( 0, -1),
+                ( 0, -2),
+                ( 0, -3),
+            ],
+            XmasDirection::SouthEast => vec![
+                (-1,  1),
+                (-2,  2),
+                (-3,  3),
+            ],
+            XmasDirection::East => vec![
+                ( 0,  1),
+                ( 0,  2),
+                ( 0,  3),
+            ],
+            XmasDirection::NorthEast => vec![
+                ( 1,  1),
+                ( 2,  2),
+                ( 3,  3),
+            ],
+        }
     }
 }
