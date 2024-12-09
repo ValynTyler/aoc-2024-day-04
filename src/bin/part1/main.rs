25dc9e3a8d1f95b5351307d08cc54e650cb6899c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 18:08:54 +0200

    feat: starting to print crossword solver

--- src/bin/part1/main.rs
+++ src/bin/part1/main.rs
@@ -1,11 +1,15 @@
 use aoc_2024_day4::xmas_crossword::XmasCrossword;
+use solved_crossword::SolvedCrossword;
 
 mod solved_crossword;
 mod xmas_direction;
 
 fn main() {
     let input_string = include_str!("../../../input/example.txt");
-    let crossword: XmasCrossword = input_string.try_into().unwrap();
 
+    let crossword: XmasCrossword = input_string.try_into().unwrap();
     println!("{}", crossword);
+
+    let solution: SolvedCrossword = crossword.into();
+    println!("{}", solution);
 }
--- src/bin/part1/solved_crossword.rs
+++ src/bin/part1/solved_crossword.rs
@@ -1,11 +1,51 @@
+use std::fmt::Display;
+use utf8_box_builder::*;
+
 use aoc_2024_day4::xmas_crossword::XmasCrossword;
 
 use crate::xmas_direction::XmasDirection;
 
 pub struct SolvedCrossword(Vec<Vec<Option<XmasDirection>>>);
 
+impl Display for SolvedCrossword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // top row
+        write!(f, "{TLC}{HL}")?;
+        self.0.iter().for_each(|_| write!(f, "{HL}{HL}").unwrap());
+        writeln!(f, "{TRC}")?;
+
+        // middle rows
+        self.0.iter().for_each(|row| {
+            write!(f, "{VL} ").unwrap();
+            row.iter().for_each(|letter| {
+                let symbol = if letter.is_some() { 'A' } else { '.' };
+                write!(f, "{symbol} ").unwrap()
+            });
+            writeln!(f, "{VL}").unwrap();
+        });
+
+        // bottom row
+        write!(f, "{BLC}{HL}")?;
+        self.0.iter().for_each(|_| write!(f, "{HL}{HL}").unwrap());
+        writeln!(f, "{BRC}")?;
+
+        Ok(())
+    }
+}
+
 impl From::<XmasCrossword> for SolvedCrossword {
     fn from(value: XmasCrossword) -> Self {
-        todo!()
+        Self(value.0
+            .iter()
+            .map(|row| {
+                row
+                    .iter()
+                    .map(|_| {
+                        None
+                    })
+                    .collect()
+            })
+            .collect()
+        )
     }
 }
