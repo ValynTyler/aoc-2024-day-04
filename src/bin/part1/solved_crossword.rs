601484da05451e4a2f9c272cfbbc86efcea2dc59
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 17:39:01 +0200

    fix: using option to better represent solution state

--- src/bin/part1/solved_crossword.rs
+++ src/bin/part1/solved_crossword.rs
@@ -2,7 +2,7 @@ use aoc_2024_day4::xmas_crossword::XmasCrossword;
 
 use crate::xmas_direction::XmasDirection;
 
-pub struct SolvedCrossword(Vec<Vec<XmasDirection>>);
+pub struct SolvedCrossword(Vec<Vec<Option<XmasDirection>>>);
 
 impl From::<XmasCrossword> for SolvedCrossword {
     fn from(value: XmasCrossword) -> Self {
