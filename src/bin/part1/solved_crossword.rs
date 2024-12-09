38c2204dcd5abe6a9babd6d9e03e1eb0b015f300
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 17:34:32 +0200

    impl some stuff on solution structs

--- src/bin/part1/solved_crossword.rs
+++ src/bin/part1/solved_crossword.rs
@@ -1,3 +1,11 @@
+use aoc_2024_day4::xmas_crossword::XmasCrossword;
+
 use crate::xmas_direction::XmasDirection;
 
 pub struct SolvedCrossword(Vec<Vec<XmasDirection>>);
+
+impl From::<XmasCrossword> for SolvedCrossword {
+    fn from(value: XmasCrossword) -> Self {
+        todo!()
+    }
+}
--- src/bin/part1/xmas_direction.rs
+++ src/bin/part1/xmas_direction.rs
@@ -8,3 +8,58 @@ pub enum XmasDirection {
     East,
     NorthEast,
 }
+
+impl XmasDirection {
+    pub fn deltas(&self) -> Vec<(isize, isize)> {
+        match self {
+            XmasDirection::North => vec![
+                ( 0,  0),
+                ( 0,  1),
+                ( 0,  2),
+                ( 0,  3),
+            ],
+            XmasDirection::NorthWest => vec![
+                (-0,  0),
+                (-1,  1),
+                (-2,  2),
+                (-3,  3),
+            ],
+            XmasDirection::West => vec![
+                (-0,  0),
+                (-1,  0),
+                (-2,  0),
+                (-3,  0),
+            ],
+            XmasDirection::SouthWest => vec![
+                (-0, -0),
+                (-1, -1),
+                (-2, -2),
+                (-3, -3),
+            ],
+            XmasDirection::South => vec![
+                ( 0, -0),
+                ( 0, -1),
+                ( 0, -2),
+                ( 0, -3),
+            ],
+            XmasDirection::SouthEast => vec![
+                ( 0,  0),
+                (-1,  1),
+                (-2,  2),
+                (-3,  3),
+            ],
+            XmasDirection::East => vec![
+                ( 0,  0),
+                ( 0,  1),
+                ( 0,  2),
+                ( 0,  3),
+            ],
+            XmasDirection::NorthEast => vec![
+                ( 0,  0),
+                ( 1,  1),
+                ( 2,  2),
+                ( 3,  3),
+            ],
+        }
+    }
+}
