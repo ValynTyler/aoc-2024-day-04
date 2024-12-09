5a2bfbc3499c937a4516ecc8edc8754b42e21e0c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 17:14:53 +0200

    fix: wrong file routing in macro

--- src/bin/part1/main.rs
+++ src/bin/part1/main.rs
@@ -1,7 +1,7 @@
 use aoc_2024_day4::xmas_crossword::XmasCrossword;
 
 fn main() {
-    let input_string = include_str!("../../input/example.txt");
+    let input_string = include_str!("../../../input/example.txt");
     let crossword: XmasCrossword = input_string.try_into().unwrap();
 
     println!("{}", crossword);
