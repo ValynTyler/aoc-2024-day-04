6ea326260b7f28a82dd7978f77c41e9d79105951
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-12 20:40:32 +0200

    feat: vec2 conversions

--- src/bin/part1/main.rs
+++ src/bin/part1/main.rs
@@ -1,14 +1,13 @@
+use aoc_2024_day4::grid::GridDirection;
 use aoc_2024_day4::xmas::{BadDataError, XmasGrid};
-use aoc_2024_day4::vec::Vec2USize as Vec2U;
 
 fn main() -> Result<(), BadDataError> {
     let input_string = include_str!("../../../input/example.txt");
     let crossword = XmasGrid::try_from(input_string)?;
     println!("{}", crossword);
 
-    for i in 0..7 {
-        println!("{:?}", crossword.grid().get(Vec2U(i, 0)));
-    }
+    let pos = GridDirection::East.delta() * 1;
+    println!("{:?} {:?}", crossword.grid().get(pos.try_into().unwrap()), pos);
 
     Ok(())
 }
--- src/vec/vec2_isize.rs
+++ src/vec/vec2_isize.rs
@@ -1,11 +1,20 @@
-use std::ops::Add;
+use std::ops::{Add, Mul};
 
+#[derive(Debug, Clone, Copy)]
 pub struct Vec2ISize(pub isize, pub isize);
 
 impl Add for Vec2ISize {
     type Output = Self;
 
-    fn add(self, rhs: Vec2ISize) -> Self::Output {
+    fn add(self, rhs: Self) -> Self::Output {
         Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl Mul::<isize> for Vec2ISize {
+    type Output = Self;
+
+    fn mul(self, rhs: isize) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs)
     }
 }
