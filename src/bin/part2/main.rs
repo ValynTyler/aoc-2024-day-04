34fdece90e0f73ead01b66934c51c80938172335
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-13 00:01:06 +0200

    update part2/main.rs

--- src/bin/part2/main.rs
+++ src/bin/part2/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    todo!();
+use aoc_2024_day4::xmas::{BadDataError, XmasGrid};
+
+fn main() -> Result<(), BadDataError> {
+    let input_string = include_str!("../../../input/example.txt");
+    let crossword = XmasGrid::try_from(input_string)?;
+    println!("{}", crossword);
+
+    Ok(())
 }
