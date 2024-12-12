b61e5ecb3c0a070f2448563c0c905fc3cb72822a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-12 18:27:17 +0200

    chore: update main function return type

--- src/bin/part1/main.rs
+++ src/bin/part1/main.rs
@@ -1,14 +1,16 @@
-use aoc_2024_day4::xmas_crossword::XmasCrossword;
+use aoc_2024_day4::xmas_crossword::{BadInputError, XmasCrossword};
 use solved_crossword::SolvedCrossword;
 
 mod solved_crossword;
 
-fn main() {
+fn main() -> Result<(), BadInputError> {
     let input_string = include_str!("../../../input/example.txt");
 
-    let crossword: XmasCrossword = input_string.try_into().unwrap();
+    let crossword: XmasCrossword = input_string.try_into()?;
     println!("{}", crossword);
 
     let solution: SolvedCrossword = crossword.into();
     println!("{}", solution);
+
+    Ok(())
 }
