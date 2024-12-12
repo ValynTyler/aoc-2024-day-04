c0addeb3bf29dc4d5714a91d92c4c03db9b341aa
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-12 23:12:42 +0200

    feat: now corretly validating crossword cell

--- src/bin/part1/main.rs
+++ src/bin/part1/main.rs
@@ -1,19 +1,43 @@
 use aoc_2024_day4::grid::GridDirection;
-use aoc_2024_day4::vec::Vec2USize;
-use aoc_2024_day4::xmas::{BadDataError, XmasGrid};
+use aoc_2024_day4::vec::{Vec2ISize, Vec2USize};
+use aoc_2024_day4::xmas::{BadDataError, XmasGrid, XmasLetter};
 
 fn main() -> Result<(), BadDataError> {
     let input_string = include_str!("../../../input/example.txt");
     let crossword = XmasGrid::try_from(input_string)?;
     println!("{}", crossword);
 
-    let pos = Vec2USize(4, 0);
-    for i in 0..4 {
-        let offset = GridDirection::SouthEast.delta() * i;
-        let offset = Vec2USize::try_from(offset).unwrap();
-        let letter = crossword.grid().get(pos + offset);
-        println!("{:?} {:?}", letter, pos + offset);
+    use XmasLetter::*;
+    use GridDirection::*;
+    let xmas = vec![X, M, A, S];
+    let directions = vec![
+        North,
+        South,
+        East,
+        West,
+        NorthEast,
+        NorthWest,
+        SouthEast,
+        SouthWest,
+    ];
+
+    let mut sum = 0;
+    for dir in directions {
+        let pos = Vec2ISize(5, 0);
+        let mut flag = true;
+        for i in 0..4 {
+            let offset = dir.delta() * i as isize;
+            if let Ok(coords) = Vec2USize::try_from(pos + offset) {
+                let letter = crossword.grid().get(coords).unwrap();
+                if letter != xmas[i] { flag = false }
+            } else {
+                flag = false
+            }
+        }
+        println!("{}", flag);
+        sum += flag as u32
     }
+    println!("{}", sum);
 
     Ok(())
 }
