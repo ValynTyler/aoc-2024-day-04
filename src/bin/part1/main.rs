55da2486e63558fde9c16830fb042d7fe3c1f681
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-12 20:47:21 +0200

    feat: now reading grid contents

--- src/bin/part1/main.rs
+++ src/bin/part1/main.rs
@@ -1,4 +1,5 @@
 use aoc_2024_day4::grid::GridDirection;
+use aoc_2024_day4::vec::Vec2USize;
 use aoc_2024_day4::xmas::{BadDataError, XmasGrid};
 
 fn main() -> Result<(), BadDataError> {
@@ -6,8 +7,13 @@ fn main() -> Result<(), BadDataError> {
     let crossword = XmasGrid::try_from(input_string)?;
     println!("{}", crossword);
 
-    let pos = GridDirection::East.delta() * 1;
-    println!("{:?} {:?}", crossword.grid().get(pos.try_into().unwrap()), pos);
+    let pos = Vec2USize(4, 0);
+    for i in 0..4 {
+        let offset = GridDirection::SouthEast.delta() * i;
+        let offset = Vec2USize::try_from(offset).unwrap();
+        let letter = crossword.grid().get(pos + offset);
+        println!("{:?} {:?}", letter, pos + offset);
+    }
 
     Ok(())
 }
